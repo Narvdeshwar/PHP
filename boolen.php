158bed2dcc40bd3582b18410e043f82f18c4995d
Ashrith <56790381+Narvdeshwar@users.noreply.github.com>
2021-12-05 14:02:22 +0530

    Boolean datatype

---
+++ boolen.php
@@ -0,0 +1,26 @@
+<?php
+//BOOLEAN DATA TYPE
+//BOOLEAN FALSE VALUE
+//$bool=FALSE;
+//$bool=false;
+//$bool='';->blank string is false
+//$bool=0;->0 is false
+//$bool=0.0;->0.0 is false
+//$bool=array();-> blank array is also false
+//$bool=NULL;->special type NULL is false
+$bool=null;
+if($bool){
+    echo "True";
+}
+else{
+    echo "False";
+}
+//BOOLEAN TRUE
+//$bool=1;
+/*if($bool){
+    echo "True";
+}
+else{
+    echo "False";
+}*/
+?>
