d648b9378e0895d67cac139333cd59d67adddf52
Ashrith <56790381+Narvdeshwar@users.noreply.github.com>
2021-12-05 08:11:18 +0530

    Variable declaration
    Variable declaration rules

---
+++ variable.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * VARIABLE DECLARATION RULE
+     * 1.Variable name start with letter or underscore.
+     * 2.Variable names can't start with number.
+     * 3.A variable name can ahve any number of letters, Underscore.
+     * 4.valid letters are all ASCII hexadecimal from 00 to FF(0-255).
+     */
+    $a="variable declaration";
+    //echo $a;
+    /**
+     * var_dump() is used for debugging in php
+     * it dumps the information about the variable
+     * in special case for string it also tells about the length of string
+     */
+    var_dump($a);
+    //   
+?>
